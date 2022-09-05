ef43102958d282cb050ca11b9d7dd0f0e91518d9
Svetlana <svetlana_el12@mail.ru>
2022-09-05 19:36:58 +0500

    edit: homePage (successMoneyTransactions, FavoritesList)

--- public/homePage.js
+++ public/homePage.js
@@ -10,7 +10,9 @@ logout.action = () => {
 
 //Получение информации о пользователе
 ApiConnector.current((profile) => {
-    ProfileWidget.showProfile(profile.data)
+    if (profile.success) {
+        ProfileWidget.showProfile(profile.data);
+    }
 });
 
 //Получение текущих курсов валюты
@@ -24,4 +26,73 @@ let getRates = setTimeout(function updateTable() {
 }, 0);
 
 //Операции с деньгами
+const managerMoney = new MoneyManager();
 
+const successMoneyTransactions = (success, data, error) => {
+    if (success) {
+        managerMoney.setMessage(success, 'Операция проведена успешно!');
+        ProfileWidget.showProfile(data);
+    } else {
+        managerMoney.setMessage(success, error)
+    }
+}
+
+//пополнение баланса
+managerMoney.addMoneyCallback = (data) => {
+    ApiConnector.addMoney({ currency: data.currency, amount: data.amount }, (response) => {
+        successMoneyTransactions(response.success, response.data, response.error);
+    })
+}
+
+//конвертирование валюты
+managerMoney.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney({ fromCurrency: data.fromCurrency, targetCurrency: data.targetCurrency, fromAmount: data.fromAmount }, (response) => {
+        successMoneyTransactions(response.success, response.data, response.error);
+    })
+}
+
+//перевод валюты
+managerMoney.sendMoneyCallback = (data) => {
+    console.log(data)
+    ApiConnector.transferMoney({ to: data.to, currency: data.currency, amount: data.amount }, (response) => {
+        successMoneyTransactions(response.success, response.data, response.error);
+    })
+}
+
+//Работа с избранным
+const widgetFavorites = new FavoritesWidget();
+
+const updateFavoritesList = (data) => {
+    widgetFavorites.clearTable();
+    widgetFavorites.fillTable(data);
+    managerMoney.updateUsersList(data);
+}
+
+//начальный список избранного
+ApiConnector.getFavorites((data) => {
+    updateFavoritesList(data.data);
+})
+
+//добавление пользователя в список избранных
+widgetFavorites.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites({ id: data.id, name: data.name }, (response) => {
+        if (response.success) {
+            updateFavoritesList(response.data);
+            widgetFavorites.setMessage(response.success, 'Пользователь добавлен успешно');
+        } else {
+            widgetFavorites.setMessage(response.success, response.error);
+        }
+    })
+}
+
+//удаление пользователя из избранного
+widgetFavorites.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, (response) => {
+        if (response.success) {
+            updateFavoritesList(response.data);
+            widgetFavorites.setMessage(response.success, 'Пользователь удален успешно');
+        } else {
+            widgetFavorites.setMessage(response.success, response.error);
+        }
+    })
+}
