760e389a50efa03dc0a5f8c1a95e7d7d726a0a95
Svetlana <svetlana_el12@mail.ru>
2022-09-05 15:57:13 +0500

    refactor: loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,25 +1,15 @@
 'use strict';
-
 const user = new UserForm();
 
 user.loginFormCallback = data => {
     ApiConnector.login({ login: data.login, password: data.password }, response => {
-        if (response.success) {
-            location.reload();
-        } else {
-            user.setLoginErrorMessage(response.error)
-        }
+        (response.success) ? location.reload() : user.setLoginErrorMessage(response.error)
     })
 }
 
 user.registerFormCallback = data => {
     ApiConnector.register({ login: data.login, password: data.password }, response => {
-        console.log(response)
-        if (response.success) {
-            location.reload();
-        } else {
-            user.setRegisterErrorMessage(response.error)
-        }
+        (response.success) ? location.reload() : user.setRegisterErrorMessage(response.error)
     })
 }
 
